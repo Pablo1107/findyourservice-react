16d1c289e8dfe14013525668c014619f0975d2cf
Pablo Andres Dealbera <pablo@bravenew.tech>
2019-03-13 16:15:01 -0300

    Start implementing Redux

--- src/scenes/Dashboard/index.js
+++ src/scenes/Dashboard/index.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import Sidebar from './components/Sidebar/index.js'
 import Services from './components/Services/index.js'
+// import { connect } from 'react-redux';
+// import { fetchAuthStatus } from 'actions/authActions.js';
 
-class App extends Component {
+class Dashboard extends Component {
+  // componentDidMount() {
+  //   this.props.fetchAuthStatus();
+  // }
+
   render() {
     const { match } = this.props;
+    // console.log(this.props.posts);
     return (
       <div className="App">
         <Sidebar />
         <Services match={match}/>
+
       </div>
     );
   }
 }
 
-export default App;
+export default Dashboard;
+// const mapStateToProps = state => ({
+//     posts: state.user,
+// });
+//
+// export default connect(mapStateToProps, { fetchAuthStatus })(Dashboard);
