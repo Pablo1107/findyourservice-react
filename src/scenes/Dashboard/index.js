b77d0735c11252b0dfb52180995b612753e40084
Pablo Andres Dealbera <pablo@bravenew.tech>
2019-03-15 16:24:59 -0300

    Get auth token from localStorage store

--- src/scenes/Dashboard/index.js
+++ src/scenes/Dashboard/index.js
@@ -3,19 +3,33 @@ import Sidebar from './components/Sidebar/index.js'
 import Services from './components/Services/index.js'
 // import { connect } from 'react-redux';
 // import { fetchAuthStatus } from 'actions/authActions.js';
+const store = require('store')
+const axios = require('axios');
 
 class Dashboard extends Component {
-  // componentDidMount() {
-  //   this.props.fetchAuthStatus();
-  // }
+  componentDidMount() {
+    // this.props.fetchAuthStatus();
+
+    this.getAuthToken();
+  }
+
+  getAuthToken() {
+    // store.clearAll();
+    const auth_token = store.get('AUTH_TOKEN');;
+    if(auth_token) {
+      axios.defaults.headers.common['Authorization'] = 
+        `Bearer ${auth_token.token}`;
+    } else {
+      this.props.history.push('/login');
+    }
+  }
 
   render() {
-    const { match } = this.props;
     // console.log(this.props.posts);
     return (
       <div className="App">
         <Sidebar />
-        <Services match={match}/>
+        <Services match={this.props.match}/>
 
       </div>
     );
