e625a25e7f4aad0ab5598b6daea10a2155b9922c
Pablo Andres Dealbera <pablo@bravenew.tech>
2019-03-22 10:46:40 -0300

    Pass history param to Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from 'history';
 // import rootReducer from './reducers'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/Main/index.js';
@@ -12,16 +13,18 @@ import store from './store.js';
 import axios from 'axios';
 axios.defaults.baseURL = 'http://homestead.test';
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
   <Provider store={store}> 
-    <Router>
+    <Router history={history}>
       <Main />
     </Router>
   </Provider>,
   document.getElementById('root')
 );
 
-if (module.hot) module.hot.accept();
+// if (module.hot) module.hot.accept();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
